fdfda2a7148ca915ca13b1ffcfa1ba6074919e4a
selma <belhadjselma1@gmail.com>
2022-04-28 15:56:10 +0100

    create database, animal table, insert data and query it

--- schema.sql
+++ schema.sql
@@ -1,5 +1,9 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    name  varchar(100),          
+    date_of_birth  date,           
+    escape_attempts int,          
+    neutered  boolean,
+    weight_kg real
 );
