897cea0139844f35162b3b91c3a71786e342dd13
selma <belhadjselma1@gmail.com>
2022-04-28 16:48:41 +0100

    add the id field to the animal table

--- data.sql
+++ data.sql
@@ -1,6 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals VALUES 
+INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES 
 ('Agumon', '2020-02-03', 0, true, 10.23 ),
 ('Gabumon', '2018-11-03', 2, true, 8 ),
 ('Pikachu', '2021-01-07', 1, false, 15.04),
--- schema.sql
+++ schema.sql
@@ -1,9 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name  varchar(100),          
-    date_of_birth  date,           
-    escape_attempts int,          
-    neutered  boolean,
-    weight_kg real
+    id SERIAL not null primary key,
+    name  varchar(100) not null,          
+    date_of_birth  date not null,           
+    escape_attempts int not null,          
+    neutered  boolean not null,
+    weight_kg real not null
 );
